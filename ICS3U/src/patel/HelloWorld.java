e0665cc752c2c765638b2846b767f4c9274a7e4b
322954991 <322954991@C-DS24WR1.tdsb.on.ca>
2016-03-02 14:44:51 -0500

    First push to git hub.

---
+++ ICS3U/src/patel/HelloWorld.java
@@ -0,0 +1,17 @@
+package patel;
+/* HelloWorld.Java
+ * This program prints "Hello World" to the console
+ * Jitesh Patel
+ * March 2, 2016
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// This prints "Hello World" to the console
+		System.out.println("Hello World");
+		
+
+	}
+
+}
