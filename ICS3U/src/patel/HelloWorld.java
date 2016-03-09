43ce5c0bdcd0ca8647a0bb2d54a48747a153b74f
322954991 <322954991@C-DS24WR1.tdsb.on.ca>
2016-03-09 14:33:46 -0500

    Mothers name program

--- ICS3U/src/patel/HelloWorld.java
+++ ICS3U/src/patel/HelloWorld.java
@@ -10,6 +10,17 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// This prints "Hello World" to the console
 		System.out.println("Hello World!!!!!!!!!!!!!");
+		System.out.println(489);
+		System.out.println("0");
+		System.out.println(-44);
+		System.out.println("4+3");
+		System.out.println(4+3);
+		System.out.println("38-9+25");
+		System.out.println(38-9+25);
+		System.out.println("26"+"18");
+		System.out.println("26"+18);
+		System.out.println(15+"31");
+		
 		
 
 	}
---
+++ ICS3U/src/patel/MotherProgram.java
@@ -0,0 +1,40 @@
+package patel;
+
+import java.util.Scanner;
+
+/* MotherProgram.Java
+ * This program determines a mothers age at birth based on her birth year and her child birth year
+ * Jitesh Patel
+ * March 8, 2016
+ */
+public class MotherProgram {
+	/**
+	 *Main Method
+	 **/
+	public static void main(String[] args) {
+		int x=5%2;
+		Scanner input = new Scanner(System.in);
+		String name; //user's name
+		int userBirth; //user's birth year
+		String motherName; //user's mother's name
+		int motherBirth; //mother's birth year
+		int motherAge;//mothers age
+
+		System.out.println("What is your name?");
+		name = input.nextLine();
+
+		System.out.println("Hello " + name + "! What year were you born in?");
+		userBirth = Integer.parseInt(input.nextLine());
+
+		System.out.println(userBirth + ", thats great! Now, what is your mothers name?");
+		motherName = input.nextLine();
+
+		System.out.println(motherName + ", that's a great name! What year was she born in?");
+		motherBirth = Integer.parseInt(input.nextLine());
+
+		motherAge = userBirth - motherBirth;
+		System.out.println(motherBirth + "? That means she was " + motherAge + " years old when you were born!");
+
+	}
+
+}
