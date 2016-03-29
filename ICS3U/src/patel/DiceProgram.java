6f3c7fccd94182349f8db35ef0974d4f8410fc21
322954991 <322954991@C-DS24WR1.tdsb.on.ca>
2016-03-29 14:35:02 -0400

    Added loop

--- ICS3U/src/patel/DiceProgram.java
+++ ICS3U/src/patel/DiceProgram.java
@@ -1,44 +1,56 @@
 package patel;
+
 /*DiceProgram.Java
  * This program rolls two dice for the user and then two for the computer and determines which roll was higher
  * Jitesh Patel
  * March 23, 2016
  */
+import java.util.Scanner;
+
 public class DiceProgram {
 
 	public static void main(String[] args) {
-		
-		//declaring variables  
-		int dieOne=(int)(Math.random()*6)+1;
-		int dieTwo=(int)(Math.random()*6)+1;
-		int x;
-		int y;
-		
-		//determining user values
-		x=dieOne+dieTwo;
-		System.out.println("You rolled: "+x);
-		
-		//re-rolling
-		dieOne=(int)(Math.random()*6)+1;
-		dieTwo=(int)(Math.random()*6)+1;
-		
-		//determining my values 
-		y=dieOne+dieTwo;
-		System.out.println("I rolled "+y);
-		
-		//deciding the victor 
-		if (x>y)
-		{
-			System.out.println("You win!");
+		boolean z = true;
+
+		while (z == true) {
+			// declaring variables
+			int dieOne = (int) (Math.random() * 6) + 1;
+			int dieTwo = (int) (Math.random() * 6) + 1;
+			int x;
+			int y;
+			String yesNo;
+
+			Scanner input = new Scanner(System.in);
+
+			// determining user values
+			x = dieOne + dieTwo;
+			System.out.println("You rolled: " + x);
+
+			// re-rolling
+			dieOne = (int) (Math.random() * 6) + 1;
+			dieTwo = (int) (Math.random() * 6) + 1;
+
+			// determining my values
+			y = dieOne + dieTwo;
+			System.out.println("I rolled " + y);
+
+			// determining the victor
+			if (x > y) {
+				System.out.println("You win! Would you like to roll again?(Y/N)");
+			} else if (x == y) {
+				System.out.println("It's a tie! Would you like to roll again?(Y/N)");
+			} else {
+				System.out.println("I win! Would you like to roll again?(Y/N)");
+			}
+			yesNo = input.nextLine();
+			if (yesNo.equals("Y")) {
+				z = true;
+			} else {
+				z = false;
+			}
+
 		}
-		else if (x==y)
-		{
-			System.out.println("It's a tie!");
-		}
-		else
-		{
-			System.out.println("I win!");
-		}
+
 	}
 
 }
