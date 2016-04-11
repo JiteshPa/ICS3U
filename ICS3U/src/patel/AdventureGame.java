9cf5200f4c4e239ac4855ac75dc500d9ab1c5800
322954991 <322954991@C-DS24WR1.tdsb.on.ca>
2016-04-11 14:57:27 -0400

    4th work day

--- ICS3U/src/patel/AdventureGame.java
+++ ICS3U/src/patel/AdventureGame.java
@@ -9,45 +9,51 @@ public class AdventureGame {
 		Console log = new Console(50, 150);
 
 		String[][] userInput = new String[2][10];
-		int gold;
+		int gold=1;
 		boolean sword;
 		String riddle;
 		int triesTroll = 2;
 		boolean death = false;
 		boolean playAgain = true;
 		String replay;
+		String userGuess;
 
 		while (playAgain == true) {
 			log.println(
 					"Run run run! You are a thief who has just stolen gold from a castle! You come across two paths. One leads to a desert, one leads to a forest. Which path do you take? (forest/desert)");
 
 			userInput[0][0] = log.readLine();
+			log.clear();
 
 			if (userInput[0][0].equalsIgnoreCase("forest")) {
 				log.println(
 						"The gaurds chase you into the forest! You get to a clearing, and see a large tree. Do you hide in the tree or keep running? (hide in tree/keep running");
 
 				userInput[0][1] = log.readLine();
-
+				log.clear();
 				if (userInput[0][1].equalsIgnoreCase("hide in tree")) {
 					log.println(
 							"You successfully climb the tree. The guards are confused, they look around trying to find a trail for you. You see some rocks up on the tree. Unsure of how they got there, you pick one up and take aim. You could throw the rock and knock the guards out, but there's only a 1/3 chance of success. Do you take the risk?");
 					userInput[0][2] = log.readLine();
-
+					log.clear();
 					if (userInput[0][2].equalsIgnoreCase("Yes")) {
 						int chance = (int) (Math.random() * 3);
 						if (chance == 0 || chance == 1) {
 							log.println("You miss, the guards spot you and kill you. You are dead.");
 							death = true;
+							
 						} else {
 							log.println(
 									"You hit the guards and kill them. You climb down and steal some of their gold, along with a sword as a weapon");
 							sword = true;
+							
 						}
 					}
+					
 					log.println(
 							"You come across a cross roads. One path leads to a bridge, the other goes to a yellow brick road. Where would you like to go, the bridge or the yellow brick road?");
 					userInput[0][3] = log.readLine();
+					log.clear();
 					if (userInput[0][3].equalsIgnoreCase("the bridge") && death == false) {
 						log.println(
 								"As you are beggening to cross, a troll blocks your path!You tell the troll that you wish to pass, and he says he'll let you if you can get his riddle right in three guess's. The riddle is: What gets wetter as it drys?");
@@ -56,7 +62,7 @@ public class AdventureGame {
 							if (riddle.equalsIgnoreCase("towel") && i == 3) {
 								log.println("The troll says 'that is correct, you may procced!'");
 								i = 0;
-
+								
 							} else if (riddle.equalsIgnoreCase("towel") && (i == 1 || i == 2)) {
 								log.println(
 										"The trool says 'correct, but you didnt get it on the first try! I know I said I'd give you 3 tries, but i lied.' The troll eats you, you are dead.");
@@ -64,6 +70,7 @@ public class AdventureGame {
 								log.println("The troll says 'wrong, try again.' You have " + i + " guesses left");
 								triesTroll--;
 							}
+							log.clear();
 
 						}
 						if (triesTroll == 0) {
@@ -75,6 +82,7 @@ public class AdventureGame {
 							} else {
 								log.println("With nothing to defend yourself, the guards kill you. You are dead.");
 							}
+							log.clear();
 						}
 					} else if (userInput[0][3].equalsIgnoreCase("yellow brick road") && death == false) {
 						log.println(
@@ -92,9 +100,103 @@ public class AdventureGame {
 			{
 				log.println("You come to a camel. Do you want to take it?");
 				userInput[1][0]=log.readLine();
+				log.clear();
 				if (userInput[1][0].equalsIgnoreCase("Yes"))
 				{
-					
+					log.println("You ride the camal until you approch a pyramid.");
+					log.println("Do you enter?");
+					userInput[1][1]=log.readLine();
+					log.clear();
+					if (userInput[1][1].equalsIgnoreCase("yes"))
+					{
+						log.println("You enter the pyramid, unaware of what awaits you.");
+						log.println("You come to a tunnel.");
+						log.println("Would you like to enter the tunnel?");
+						userInput[1][2]=log.readLine();
+						log.clear();
+						if (userInput[1][2].equalsIgnoreCase("yes"))
+						{
+							log.println("You see a bag of gold being held by a dead corpse.");
+							log.println("You take it and move on.");
+							gold=gold++;
+						}
+						log.println("You keep walking until you come across a mummy.");
+						log.println("The mummy says:");
+						log.println("'All who enter here DIE!... Unless you can guess my number thats between 1 and 5'");
+						log.println("You have three guesses.");
+						
+						
+						int mummyNumber = (int) (Math.random() * 3);
+						for (int i=3;i!=0;i--)
+						{
+							userGuess=log.readLine();
+							if (userGuess.equals(mummyNumber)&&i==3)
+							{
+								i=0;
+								log.println("Wow... you got it on the first try.");
+								log.println("You must be a genuis!");
+								log.println("I need a genuis like you, to lead my army of skeletons!");
+								log.println("Go, lead my army and attack that wretched kingdom!");
+								log.println("Power hungry, you lead the army to the kingdom.");
+								log.println("You need a battle plan!");
+								log.println("Will you lay seige to the castle, or assult it head on?(seige/assult)");
+								userInput[1][3]=log.readLine();
+								log.clear();
+								if(userInput[1][3].equalsIgnoreCase("seige"))
+								{
+									log.println("You lay seige for three gruleng months, until the kingdom finally surrender.");
+									log.println("You take over as the new king, marry the princess, and live happily ever after...");
+									Thread.sleep(2000);
+									log.println("For all of two hours until the mummy comes back demanding the throne.");
+									log.println("He kills you.");
+									log.println("You are dead.");
+									death=true;
+									
+								}
+								else if(userInput[1][3].equalsIgnoreCase("assult"))
+								{
+									log.println("You charge your army at the castle.");
+									log.println("As bones start flying at you, you realize that all it takes is one hit to a skelleton to kill it.");
+									log.println("The defenders of the castle kill your army and you");
+									log.println("You are dead.");
+									death=true;
+								}
+								else
+								{
+									log.println("Please enter a valid input.");
+								}
+							}
+							else if (userGuess.equals(mummyNumber)&&i==2)
+							{
+								log.println("That is correct, you may leave.");
+								log.println("As you leave the pyramid, you come across a random portal.");
+								log.println("Do you use it?");
+								userInput[1][4]=log.readLine();
+								if (userInput[1][4].equalsIgnoreCase("yes"))
+								{
+									log.println("You use the portal and it drops you off at home.");
+								}
+								else
+								{
+									log.println("You keep walking for a while until you begen to feel thursty.");
+									log.println("You died of dehydration.");
+									death=true;
+								}
+				
+							}
+							else if (userGuess.equals(mummyNumber)&&i==1)
+							{
+								log.println("That is correct... however... I think I'm going to kill you anyways.");
+								log.println("Yoou are dead.");
+								death=true;
+							}
+							else
+							{
+								log.println("That's wrong.");
+								log.println("You have "+i+" tries left.");
+							}
+						}
+					}
 				}
 				else
 				{
@@ -114,7 +216,7 @@ public class AdventureGame {
 
 				
 			}
-			
+			log.println("You accumulated "+gold+" gold on your journey.");
 			log.println("Would you like to play again?");
 			replay=log.readLine();
 			if (replay.equalsIgnoreCase("No"))
